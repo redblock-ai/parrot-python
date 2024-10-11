a0d2ed48a56e722e48cee8bebb592f4c82d87ae7
Harsha <harsha@redblock.ai>
2024-10-11 11:55:29 -0700

    OpenAI adapter class defined along with custom exceptions for exception handling.

--- parrot/parrot_openai.py
+++ parrot/parrot_openai.py
@@ -2,5 +2,57 @@
 A class which serves as a Adapter for OpenAI GPT model inference over PARROT testset.
 """
 
+class OpenAdapterException(Exception):
+    """
+    Base class for Custom Exceptions within Ollama Adapter.
+    """
+class ExceptionGroup:
+    class InvalidDataset(OpenAdapterException):
+        """Exception raised for invalid dataset access object"""
+        def __init__(self, message="Invalid Dataset Object passed."):
+            self.message = message
+            super().__init__(self.message)
+    
+    class InvalidDataframe(OpenAdapterException):
+        """Exception raised for invalid dataframe"""
+        def __init__(self, message="Invalid Dataset Object passed."):
+            self.message = message
+            super().__init__(self.message)
+
+    class ModelNameCannotBeNone(OpenAdapterException):
+        def __init__(self, message="Invalid model passed for inference."):
+            self.message = message
+            super().__init__(self.message)
+    
+    class ModelNameCannotBeEmpty(OpenAdapterException):
+        def __init__(self, message="Paramter 'model_name' cannot be empty."):
+            self.message = message
+            super().__init__(self.message)
+
+    class PromptCannotBeNone(OpenAdapterException):
+        def __init__(self, message="Paramter 'prompt' passed cannot be of type None."):
+            self.message = message
+            super().__init__(self.message)
+    
+    class PromptCannotBeEmpty(OpenAdapterException):
+        def __init__(self, message="Paramter 'prompt' cannot be empty."):
+            self.message = message
+            super().__init__(self.message)
+
+    class InsufficientBatchSize(OpenAdapterException):
+        def __init__(self, message="Paramter 'prompt' cannot be empty."):
+            self.message = message
+            super().__init__(self.message)
+
 class OpenAdapter:
-    pass
+    """
+    A custom-class, that servers as a adapter for model inference over PARROT-datasets using OpenAI endpoint on top of langchain.
+    """
+    def __init__(self, 
+    dataset:Datasets = None,
+    model_name: str = None,
+    prompt: str = None,
+    temperature: float = 0,
+    top_k: int = 10,
+    top_n: float = 0.2) -> None:
+        pass
